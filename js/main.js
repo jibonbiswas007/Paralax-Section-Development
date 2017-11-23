a0a8269b5a478c11952a1cfb4ee70a1799b6d344
jibonbiswas007 <33145139+jibonbiswas007@users.noreply.github.com>
2017-11-23 22:05:35 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+(function($) {
+    "use strict";
+    
+    jQuery(document).ready(function($){
+        
+        var s = skrollr.init({
+            render: function(data) {
+                //Debugging - Log the current scroll position.
+                //consol.log(data.curTop);
+            }
+        });
+        
+    });
+    
+    jQuery(window).load(function(){
+        
+    });
+    
+    
+}(jQuery));
